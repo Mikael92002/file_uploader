2c0011094646a8097ed46a62f0adc6a3efd9b86d
Mikael <174565360+Mikael92002@users.noreply.github.com>
2026-02-27 14:18:23 -0600

    Refactored folder BFS

--- client/src/utils/functions.tsx
+++ client/src/utils/functions.tsx
@@ -1,26 +1,26 @@
 import type { Folder } from "../types/types";
 
 export function flatFolderArrayToNestedArray(arr: Folder[]) {
-  const map = new Map<number | null, Array<Folder>>();
+  const map = new Map<number | null, Folder[]>();
   for (const folder of arr) {
-    if (map.get(folder.parentId)) {
-      map.get(folder.parentId)?.push(folder);
+    const existingArr = map.get(folder.parentId);
+    if (existingArr) {
+      existingArr.push(folder);
     } else {
       map.set(folder.parentId, [folder]);
     }
   }
-  let q = map.get(null) || [];
-  const currArray = map.get(null);
+  const root = map.get(null);
+  let q = root || [];
   while (q.length > 0) {
-    let nextLevel: Folder[] = [];
+    const nextLevel: Folder[] = [];
     for (let i = 0; i < q.length; i++) {
       const parentId = q[i].id;
       const childrenFromParentId = map.get(parentId);
-      console.log(`parentId: ${parentId}, children:`, childrenFromParentId);
       q[i].children = childrenFromParentId ? childrenFromParentId : [];
-      nextLevel = [...nextLevel, ...q[i].children];
+      nextLevel.push(...q[i].children);
     }
     q = nextLevel;
   }
-  return currArray![0];
+  return root![0];
 }
