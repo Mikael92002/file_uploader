2798a25a5353f6965d40ba7ace460a6ecbc37d1b
Mikael <174565360+Mikael92002@users.noreply.github.com>
2026-03-06 17:23:42 -0600

    Added prisma generated folder to gitignore

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,7 @@ import expressSession from "express-session";
 import "./middleware/auth";
 import passport from "passport";
 import { fileRouter, authRouter, userRoute, folderRouter } from "./routes";
+import cors from "cors";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
@@ -15,14 +16,31 @@ const prisma = new PrismaClient({ adapter });
 
 const app = express();
 
+// 1. security:
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true, // for passport.js
+  }),
+);
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
+
+// 2. parsers:
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public")); // to upload files locally
+app.use(express.static("public"));
 
+// 3. session:
 app.use(
   expressSession({
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
     },
     secret: process.env.SECRET!,
     resave: true,
@@ -34,23 +52,18 @@ app.use(
   }),
 );
 
+
+// 4. passport:
 app.use(passport.initialize());
 app.use(passport.session());
 
+// 5. routes:
 app.use("/api/user", userRoute);
 app.use("/api/file", fileRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/folder", folderRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("/{*splat}", (req: Request, res: Response) => {
-    res.sendFile(
-      path.join(__dirname, "../client/dist", "index.html"),
-    );
-  });
-}
-
+// 6. error handler:
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   const statusCode = err.status || 500;
